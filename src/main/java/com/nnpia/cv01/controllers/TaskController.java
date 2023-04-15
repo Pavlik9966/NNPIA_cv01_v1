90e07e37a08be477fbf17f0978d76f4c624a6aca
Pavlik9966 <pepa.maxa@seznam.cz>
2023-04-15 19:12:17 +0200

    Cv08

--- src/main/java/com/nnpia/cv01/controllers/TaskController.java
+++ src/main/java/com/nnpia/cv01/controllers/TaskController.java
@@ -4,6 +4,7 @@ import com.nnpia.cv01.services.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,13 @@ public class TaskController {
     @GetMapping("")
     public ResponseEntity<?> getAllTasks() {
         var result = taskService.findAllTasks();
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAllTasksByAppUserId(@PathVariable final Long id) {
+        var result = taskService.findAllTasksByAppUserId(id);
 
         return ResponseEntity.ok(result);
     }
